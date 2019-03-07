f1747f9b8747176da9580c2c17dd7a95d6d08468
JulenP <julencrossunited@gmail.com>
2019-03-07 14:26:46 +0100

    TweetMain inside converters implemented

--- src/converters/tweet/TweetMain.java
+++ src/converters/tweet/TweetMain.java
@@ -1,5 +1,39 @@
 package converters.tweet;
 
-public class TweetMain {
+import java.io.File;
+import java.io.IOException;
 
+import weka.core.Instances;
+import weka.core.converters.CSVLoader;
+
+
+import static utils.Utils.saveInstances;
+
+public class TweetMain {
+	public static void main(String[] args) {
+        if (args.length != 2) {
+            System.err.println("El programa necesita 2 argumentos:\n1) Ruta de la carpeta para datos.\n2) Ruta arff.\n");
+            System.exit(1);
+        }
+        String csvPath = args[0];
+        String outputArffPath = args[1];
+        CSVLoader loader =new CSVLoader();
+        try {
+			loader.setSource(new File(csvPath));
+		} catch (IOException e) {
+			System.out.println("error al cargar el archivo csv");
+			e.printStackTrace();
+			System.exit(1);
+		}
+        Instances data=null;
+        try {
+			data=loader.getDataSet();
+		} catch (IOException e) {
+			System.out.println("error al convertir en arff ");
+			e.printStackTrace();
+			System.exit(1);
+		}
+        saveInstances(data,outputArffPath);
+        
+	}
 }
