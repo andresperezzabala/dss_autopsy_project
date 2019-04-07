42499be124980f89c3deda155d79afc83fd82a77
Ander Raso Vázquez <anderraso@gmail.com>
2019-04-07 18:22:35 +0200

    calidades de naive bayes
    Signed-off-by: Ander Raso Vázquez <anderraso@gmail.com>

---
+++ src/textmining/EstimacionCalidadHoldOutNaiveBayes.java
@@ -0,0 +1,68 @@
+package textmining;
+
+import weka.classifiers.Evaluation;
+import weka.classifiers.bayes.NaiveBayes;
+import weka.classifiers.functions.MultilayerPerceptron;
+import weka.core.Instances;
+
+import java.util.Random;
+
+import static utils.Utils.*;
+
+public class EstimacionCalidadHoldOutNaiveBayes {
+
+    public static void main(String[] args) {
+        if (args.length != 2) {
+            System.err.println("El programa necesita 2 argumentos:\n1) Ruta del train_bow_as.arff.\n2) Ruta del txt con la evaluacion a guardar.\n");
+            System.exit(1);
+        }
+
+        String trainPath = args[0];
+        String resultsPath = args[1];
+
+        Instances instances = null;
+        try {
+            instances = loadInstances(trainPath);
+            instances.setClass(instances.attribute("gs_text34"));
+            instances.randomize(new Random(1));
+        } catch (Exception e) {
+            System.out.println("Error al cargar el archivo: " + trainPath);
+            e.printStackTrace();
+            System.exit(1);
+        }
+
+        Instances train = null;
+        Instances test = null;
+        try {
+            train = getTrain(70.0, instances);
+            test = getTest(70.0, instances);
+            train.setClass(train.attribute("gs_text34"));
+            test.setClass(test.attribute("gs_text34"));
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("Error al hacer la particion hold out.");
+            System.exit(1);
+        }
+
+        NaiveBayes cls = new NaiveBayes();
+        Evaluation eval = null;
+        try {
+            eval = holdOutEval(cls, train, test);
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("Error al hacer hold out.");
+            System.exit(1);
+        }
+
+        String results = null;
+        try {
+            results = getEvaluationResults(eval);
+        } catch (Exception e) {
+            System.out.println("Error al intentar sacar resultados de la evaluacion");
+            e.printStackTrace();
+            System.exit(1);
+        }
+
+        printToFile(results, resultsPath);
+    }
+}
---
+++ src/textmining/EstimacionCalidadNoHonestaNaiveBayes.java
@@ -0,0 +1,53 @@
+package textmining;
+
+import weka.classifiers.Evaluation;
+import weka.classifiers.bayes.NaiveBayes;
+import weka.classifiers.functions.MultilayerPerceptron;
+import weka.core.Instances;
+
+import static utils.Utils.*;
+
+public class EstimacionCalidadNoHonestaNaiveBayes {
+
+    public static void main(String[] args) {
+        if (args.length != 2) {
+            System.err.println("El programa necesita 2 argumentos:\n1) Ruta del train_bow_as.arff.\n2) Ruta del txt con la evaluacion a guardar.\n");
+            System.exit(1);
+        }
+
+        String trainPath = args[0];
+        String resultsPath = args[1];
+
+        Instances train = null;
+        try {
+            train = loadInstances(trainPath);
+            train.setClass(train.attribute("gs_text34"));
+        } catch (Exception e) {
+            System.out.println("Error al cargar el archivo: " + trainPath);
+            e.printStackTrace();
+            System.exit(1);
+        }
+
+        NaiveBayes cls = new NaiveBayes();
+
+        Evaluation eval = null;
+        try {
+            eval = noHonestEval(cls, train);
+        } catch (Exception e) {
+            System.out.println("Error al evaluar");
+            e.printStackTrace();
+            System.exit(1);
+        }
+
+        String results = null;
+        try {
+            results = getEvaluationResults(eval);
+        } catch (Exception e) {
+            System.out.println("Error al intentar sacar resultados de la evaluacion");
+            e.printStackTrace();
+            System.exit(1);
+        }
+
+        printToFile(results, resultsPath);
+    }
+}
